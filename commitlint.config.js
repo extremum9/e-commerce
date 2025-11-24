073de436e5b2bca621e58c2578b13712ea02c707
extremum9 <denprogman99@gmail.com>
2025-11-24 14:28:04 +0200

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'a11y',
+        'feat',
+        'fix',
+        'test',
+        'build',
+        'chore',
+        'perf',
+        'docs',
+        'refactor',
+        'style',
+        'ci',
+        'revert',
+      ],
+    ],
+  },
+};
