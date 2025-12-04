b4dd7b5ac758d88b1e7b9d0a9976ebd32b45220e
extremum9 <denprogman99@gmail.com>
2025-12-04 23:35:23 +0200

    feat(navbar): 🎸 add toolbar with brand and actions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `<h1>Welcome to {{ title() }}!</h1>`,
-  styles: [],
+  template: `<app-navbar />`,
+  imports: [Navbar],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {
-  protected readonly title = signal('e-commerce');
-}
+export class App {}
