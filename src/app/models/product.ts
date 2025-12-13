215cee2dc8954334f153c1fcb92bdb74c26a136b
extremum9 <denprogman99@gmail.com>
2025-12-13 08:42:55 +0200

    feat: 🎸 create mock products

---
+++ src/app/models/product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  price: number;
+  imageUrl: string;
+  rating: number;
+  reviewCount: number;
+  inStock: boolean;
+  favorite: boolean;
+};
