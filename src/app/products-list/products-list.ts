40fbb87c2e9513ef72efc598d3888272647342a6
extremum9 <denprogman99@gmail.com>
2025-12-14 23:07:46 +0200

    feat(products-list): 🎸 render list of categories

--- src/app/products-list/products-list.ts
+++ src/app/products-list/products-list.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
 
 import { PRODUCTS } from '../data/products';
 import { ProductCard } from '../product-card/product-card';
@@ -6,10 +9,19 @@ import { ProductCard } from '../product-card/product-card';
 @Component({
   selector: 'app-products-list',
   template: `
-    <div class="p-5">
-      <div class="mb-6">
-        <h2 class="text-2xl font-bold text-gray-900">{{ category() }}</h2>
-      </div>
+    <div class="p-5 md:p-8">
+      <ul class="flex flex-wrap justify-center gap-3 mb-6">
+        @for (cat of categories(); track cat) {
+          <li>
+            <a
+              [routerLink]="['/products', cat]"
+              [matButton]="cat === category() ? 'filled' : 'outlined'"
+            >
+              {{ cat | titlecase }}
+            </a>
+          </li>
+        }
+      </ul>
 
       <ul class="fluid-grid">
         @for (product of products(); track product.id) {
@@ -20,14 +32,16 @@ import { ProductCard } from '../product-card/product-card';
       </ul>
     </div>
   `,
-  imports: [ProductCard],
+  imports: [ProductCard, MatButton, TitleCasePipe, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class ProductsList {
+  protected readonly categories = signal(['all', 'electronics', 'clothing', 'accessories', 'home']);
+
   protected readonly category = input<string>();
 
   protected readonly products = computed(() => {
-    const category = this.category();
+    const category = this.category()?.toLowerCase();
 
     return category === 'all'
       ? PRODUCTS
