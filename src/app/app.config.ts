cf3992b40fcefe93e72706f23c18626849e1724f
extremum9 <denprogman99@gmail.com>
2025-12-21 22:05:41 +0200

    feat: 🎸 configure firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,11 @@ import {
   provideZonelessChangeDetection
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
 
 import { APP_ROUTES } from './routes';
 
@@ -11,6 +16,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(APP_ROUTES, withComponentInputBinding())
+    provideRouter(APP_ROUTES, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
