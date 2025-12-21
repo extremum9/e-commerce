9d03b3710125b2c5aa436f38ecc3c4bce1eaab80
extremum9 <denprogman99@gmail.com>
2025-12-21 22:05:41 +0200

    feat: 🎸 configure firebase

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  projectId: 'angular-ecommerce-b4854',
+  appId: '1:694348623180:web:7072e3db141da92e61b3e9',
+  storageBucket: 'angular-ecommerce-b4854.firebasestorage.app',
+  apiKey: 'AIzaSyDqO1_9bid-rSHW_vn2M75twaqijqUWXpk',
+  authDomain: 'angular-ecommerce-b4854.firebaseapp.com',
+  messagingSenderId: '694348623180',
+  projectNumber: '694348623180',
+  version: '2'
+};
