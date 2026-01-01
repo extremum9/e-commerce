f80ae245c8ea926f8022a6f5aad0f60dd5c88754
extremum9 <denprogman99@gmail.com>
2026-01-01 14:24:11 +0200

    feat(auth): 🎸 render login form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 import { Navbar } from './navbar/navbar';
 
@@ -12,4 +14,10 @@ import { Navbar } from './navbar/navbar';
   imports: [Navbar, RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {}
+export class App {
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+    iconRegistry.addSvgIcon('google', sanitizer.bypassSecurityTrustResourceUrl('google-logo.svg'));
+  }
+}
