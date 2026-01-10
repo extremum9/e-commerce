49d52bc35316917152ffc06e7a2c134a4d835f54
extremum9 <denprogman99@gmail.com>
2026-01-10 23:24:04 +0200

    test: 💍 fix app unit tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,30 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection, signal } from '@angular/core';
 import { provideRouter, RouterOutlet } from '@angular/router';
 import { By } from '@angular/platform-browser';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 
 import { App } from './app';
 import { Navbar } from './navbar/navbar';
+import { AuthApiClient } from './auth/auth-api-client';
+import { CurrentUser } from './models/current-user';
 
 describe(App.name, () => {
   const setup = () => {
+    const currentUser = signal<CurrentUser | null | undefined>(null);
+    const authApiClientSpy = jasmine.createSpyObj<AuthApiClient>('AuthApiClient', [], {
+      currentUser
+    });
+
     TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection(), provideRouter([])]
+      imports: [MatIconTestingModule],
+      providers: [
+        provideZonelessChangeDetection(),
+        provideRouter([]),
+        {
+          provide: AuthApiClient,
+          useValue: authApiClientSpy
+        }
+      ]
     });
     const fixture = TestBed.createComponent(App);
     const debugElement = fixture.debugElement;
     fixture.detectChanges();
 
-    return { fixture, debugElement };
+    return { fixture, debugElement, currentUser };
   };
 
-  it('should have a navbar', () => {
-    const { debugElement } = setup();
-    expect(debugElement.query(By.directive(Navbar))).toBeTruthy();
+  it('should display nothing if the user is undefined', () => {
+    const { fixture, debugElement, currentUser } = setup();
+    currentUser.set(undefined);
+
+    fixture.detectChanges();
+
+    expect(debugElement.query(By.directive(Navbar))).toBeFalsy();
+    expect(debugElement.query(By.directive(RouterOutlet))).toBeFalsy();
   });
 
-  it('should have a router outlet', () => {
+  it('should display a navbar along with a router outlet if the user is defined', () => {
     const { debugElement } = setup();
+
+    expect(debugElement.query(By.directive(Navbar))).toBeTruthy();
     expect(debugElement.query(By.directive(RouterOutlet))).toBeTruthy();
   });
 });
