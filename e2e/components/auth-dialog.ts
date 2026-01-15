22b77c28efd3e092f30d5c9bb00387cbbefcd5ea
extremum9 <denprogman99@gmail.com>
2026-01-15 21:49:53 +0200

    test(e2e): 💍 validate login flow

--- e2e/components/auth-dialog.ts
+++ e2e/components/auth-dialog.ts
@@ -5,65 +5,62 @@ export type LoginCredentials = {
   password: string;
 };
 
-export type RegisterCredentials = LoginCredentials & {
+export type RegisterCredentials = {
   name: string;
+  email: string;
+  password?: string;
 };
 
 export class AuthDialog {
   public readonly page: Page;
 
-  public readonly signInTab: Locator;
   public readonly signUpTab: Locator;
 
-  public readonly loginEmailInput: Locator;
-  public readonly loginPasswordInput: Locator;
-  public readonly loginRememberMeCheckbox: Locator;
-  public readonly loginSubmitButton: Locator;
-  public readonly loginWithGoogleButton: Locator;
-
-  public readonly registerNameInput: Locator;
-  public readonly registerEmailInput: Locator;
-  public readonly registerPasswordInput: Locator;
-  public readonly registerSubmitButton: Locator;
-
+  public readonly nameInput: Locator;
+  public readonly emailInput: Locator;
+  public readonly passwordInput: Locator;
   public readonly passwordVisibilityToggleButton: Locator;
+  public readonly rememberMeCheckbox: Locator;
+  public readonly errorMessage: Locator;
+  public readonly submitButton: Locator;
 
   public readonly resetPasswordButton: Locator;
   public readonly resetPasswordSpinner: Locator;
 
+  public readonly loginWithGoogleButton: Locator;
+
   constructor(page: Page) {
     this.page = page;
 
-    this.signInTab = page.getByRole('tab', { name: 'Sign In' });
     this.signUpTab = page.getByRole('tab', { name: 'Sign Up' });
 
-    this.loginEmailInput = page.getByTestId('login-form-email-input');
-    this.loginPasswordInput = page.getByTestId('login-form-password-input');
-    this.loginRememberMeCheckbox = page.getByTestId('login-form-remember-me-checkbox');
-    this.loginSubmitButton = page.getByTestId('login-form-submit-button');
-    this.loginWithGoogleButton = page.getByTestId('login-with-google-button');
-
-    this.registerNameInput = page.getByTestId('register-form-name-input');
-    this.registerEmailInput = page.getByTestId('register-form-email-input');
-    this.registerPasswordInput = page.getByTestId('register-form-password-input');
-    this.registerSubmitButton = page.getByTestId('register-form-submit-button');
-
+    this.nameInput = page.getByPlaceholder('Enter your name');
+    this.emailInput = page.getByPlaceholder('Enter your email');
+    this.passwordInput = page.getByPlaceholder('Enter your password');
     this.passwordVisibilityToggleButton = page.getByTestId('password-visibility-toggle-button');
+    this.rememberMeCheckbox = page.getByTestId('login-form-remember-me-checkbox');
+    this.submitButton = page.locator('button[type=submit]');
+    this.errorMessage = page.locator('mat-error');
 
     this.resetPasswordButton = page.getByTestId('reset-password-button');
     this.resetPasswordSpinner = page.getByTestId('loading-reset-password-spinner');
+
+    this.loginWithGoogleButton = page.getByTestId('login-with-google-button');
   }
 
-  public async login({ email, password }: LoginCredentials): Promise<void> {
-    await this.loginEmailInput.fill(email);
-    await this.loginPasswordInput.fill(password);
-    await this.loginSubmitButton.click();
+  public async login({
+    email = 'john.doe@mail.com',
+    password = '123456'
+  }: Partial<LoginCredentials> = {}): Promise<void> {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.submitButton.click();
   }
 
-  public async register({ name, email, password }: RegisterCredentials): Promise<void> {
-    await this.registerNameInput.fill(name);
-    await this.registerEmailInput.fill(email);
-    await this.registerPasswordInput.fill(password);
-    await this.registerSubmitButton.click();
+  public async register({ name, email, password = '123456' }: RegisterCredentials): Promise<void> {
+    await this.nameInput.fill(name);
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.submitButton.click();
   }
 }
