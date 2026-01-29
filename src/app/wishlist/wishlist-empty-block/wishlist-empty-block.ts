846c13762739ce9e74abd3e2ccb4637be58e46d1
extremum9 <denprogman99@gmail.com>
2026-01-29 15:44:47 +0200

    feat(wishlist): 🎸 add header

--- src/app/wishlist/wishlist-empty-block/wishlist-empty-block.ts
+++ src/app/wishlist/wishlist-empty-block/wishlist-empty-block.ts
@@ -10,7 +10,7 @@ import { RouterLink } from '@angular/router';
       <div class="flex items-center justify-center w-20 h-20 mb-8 rounded-full bg-gray-100">
         <mat-icon class="scale-150 text-gray-400">favorite_border</mat-icon>
       </div>
-      <h2 class="mb-3 text-2xl font-bold text-gray-900">Your wishlist is empty</h2>
+      <h1 class="mb-3 text-2xl font-bold text-gray-900">Your wishlist is empty</h1>
       <p class="mb-8 text-gray-600">Save products by tapping the heart icon.</p>
       <a matButton="filled" routerLink="/products">Start Shopping</a>
     </div>
