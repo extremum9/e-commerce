b9f54aa09acfc6e625e28c9514362540d4a533e8
extremum9 <denprogman99@gmail.com>
2026-02-12 16:45:04 +0200

    test(e2e): 💍 validate wishlist back button, title and count

--- e2e/specs/wishlist.e2e-spec.ts
+++ e2e/specs/wishlist.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '../fixtures';
+import { ProductCard } from '../components';
 
 test.describe('Wishlist page', () => {
   test.describe('Non-authenticated user', () => {
@@ -17,16 +18,54 @@ test.describe('Wishlist page', () => {
         await expect(wishlistPage.emptyLink).toContainText('Start Shopping');
       });
     });
+
+    test.describe('Wishlist has items', () => {
+      test.beforeEach(async ({ productsPage, wishlistPage }) => {
+        await productsPage.goto();
+        await new ProductCard(productsPage.productCards.first()).toggleWishlistButton.click();
+        await new ProductCard(productsPage.productCards.nth(1)).toggleWishlistButton.click();
+        await wishlistPage.goto();
+      });
+
+      test('should display back button, title and count', async ({ wishlistPage }) => {
+        await expect(wishlistPage.backButton).toBeVisible();
+        await expect(wishlistPage.backButton).toHaveAttribute('href', '/products');
+        await expect(wishlistPage.backButton).toContainText('Continue Shopping');
+
+        await expect(wishlistPage.title).toBeVisible();
+        await expect(wishlistPage.title).toContainText('My Wishlist');
+
+        await expect(wishlistPage.count).toBeVisible();
+        await expect(wishlistPage.count).toContainText('2 items');
+      });
+    });
   });
 
   test.describe('Authenticated user', () => {
+    test.beforeEach(async ({ register }) => {
+      await register();
+    });
+
     test.describe('Wishlist is empty', () => {
-      test('should display empty block', async ({ register, wishlistPage }) => {
-        await register();
+      test('should display empty block', async ({ wishlistPage }) => {
         await wishlistPage.goto();
 
         await expect(wishlistPage.emptyTitle).toBeVisible();
         await expect(wishlistPage.emptyLink).toBeVisible();
+      });
+    });
+
+    test.describe('Wishlist has items', () => {
+      test.beforeEach(async ({ productsPage, wishlistPage }) => {
+        await new ProductCard(productsPage.productCards.first()).toggleWishlistButton.click();
+        await new ProductCard(productsPage.productCards.nth(1)).toggleWishlistButton.click();
+        await wishlistPage.goto();
+      });
+
+      test('should display back button, title and count', async ({ wishlistPage }) => {
+        await expect(wishlistPage.backButton).toBeVisible();
+        await expect(wishlistPage.title).toBeVisible();
+        await expect(wishlistPage.count).toBeVisible();
       });
     });
   });
