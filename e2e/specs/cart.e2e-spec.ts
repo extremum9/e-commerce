5ed7703d822c9089ce2c1d54003dcbf6c4f7de04
extremum9 <denprogman99@gmail.com>
2026-03-16 23:10:35 +0200

    test(e2e): 💍 validate wishlist preview presence

--- e2e/specs/cart.e2e-spec.ts
+++ e2e/specs/cart.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from '../fixtures';
+import { ProductCard } from '../components';
 
 test.describe('Cart page', () => {
   test('should display title and back button', async ({ cartPage }) => {
@@ -10,5 +11,46 @@ test.describe('Cart page', () => {
 
     await expect(cartPage.title).toBeVisible();
     await expect(cartPage.title).toContainText('Shopping Cart');
+  });
+
+  test('should display wishlist preview and move all items from wishlist to cart', async ({
+    cartPage,
+    productsPage,
+    navbar
+  }) => {
+    await cartPage.goto();
+
+    await expect(cartPage.wishlistPreviewTitle).toBeVisible();
+    await expect(cartPage.wishlistPreviewTitle).toContainText('Wishlist (0)');
+
+    await expect(cartPage.wishlistPreviewDescription).toBeVisible();
+    await expect(cartPage.wishlistPreviewDescription).toContainText(
+      'You have 0 items saved for later'
+    );
+
+    await expect(cartPage.wishlistPreviewLink).toBeVisible();
+    await expect(cartPage.wishlistPreviewLink).toContainText('View All');
+
+    await expect(cartPage.wishlistPreviewMoveAllButton).toBeHidden();
+
+    await productsPage.goto();
+    await new ProductCard(productsPage.cards.first()).toggleWishlistButton.click();
+    await new ProductCard(productsPage.cards.nth(1)).toggleWishlistButton.click();
+    await cartPage.goto();
+
+    await expect(cartPage.wishlistPreviewTitle).toContainText('Wishlist (2)');
+    await expect(cartPage.wishlistPreviewDescription).toContainText(
+      'You have 2 items saved for later'
+    );
+    await expect(cartPage.wishlistPreviewMoveAllButton).toBeVisible();
+    await expect(cartPage.wishlistPreviewMoveAllButton).toContainText('Move All to Cart');
+    await expect(cartPage.products).toHaveCount(0);
+
+    await cartPage.wishlistPreviewMoveAllButton.click();
+
+    await expect(navbar.wishlistLink).toContainText('0');
+    await expect(cartPage.wishlistPreviewMoveAllButton).toBeHidden();
+
+    await expect(cartPage.products).toHaveCount(2);
   });
 });
